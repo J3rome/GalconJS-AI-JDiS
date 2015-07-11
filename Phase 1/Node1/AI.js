8aaef9b9d5862246180dea8e15db3607d676b1a2
j3rome <jerome.abdelnour@usherbrooke.ca>
2015-07-11 09:47:40 -0400

    *Restruct

---
+++ Phase 1/Node1/AI.js
@@ -0,0 +1,22 @@
+﻿// Set your team name here!!!
+var name = "Bot";
+var game;
+
+var set_game = function (obj) {
+  game = obj;
+}
+
+var update = function (game_objects) {
+  console.log("update");
+};
+
+var set_name = function () {
+  console.log("Setting Name");
+  game.set_name("St0ners");
+};
+
+module.exports = {
+  set_name: set_name,
+  update: update,
+  set_game: set_game
+};
