7698b6cbd27193580625df8dd6bc95fa8fa0ee60
j3rome <jerome.abdelnour@usherbrooke.ca>
2015-07-11 10:39:44 -0400

    * format indent

--- Phase 1/Node1/AI.js
+++ Phase 1/Node1/AI.js
@@ -4,22 +4,22 @@ var game;
 var helpers = require("./helpers");
 
 var set_game = function (obj) {
-  game = obj;
+    game = obj;
     helpers.init(name);
 }
 
 var update = function (game_objects) {
-  console.log("update");
-  helpers.parseGameObject(game_objects);
+    console.log("update");
+    helpers.parseGameObject(game_objects);
 };
 
 var set_name = function () {
-  console.log("Setting Name");
-  game.set_name(name);
+    console.log("Setting Name");
+    game.set_name(name);
 };
 
 module.exports = {
-  set_name: set_name,
-  update: update,
-  set_game: set_game
+    set_name: set_name,
+    update: update,
+    set_game: set_game
 };
--- Phase 1/Node1/helpers.js
+++ Phase 1/Node1/helpers.js
@@ -1,11 +1,11 @@
 var _ = require("underscore");
-var myTeam;
+var teamName;
 
 module.exports = {
-    init: function(myName){
+    init: function (myName) {
         teamName = myName;
     },
-  parseGameObject : function(game){
+    parseGameObject: function (game) {
         var parsedGame = {
             myShips: [],
             neutralShips: [],       // DOESNT EXIST !
@@ -15,64 +15,64 @@ module.exports = {
             enemiesPlanets: []
         };
 
-      if(game){
-          if(game.ships){
-              _.each(game.ships, function(ship){
-                  if(ship.position){
-                      ship.position.x = parseFloat(ship.position.x);
-                      ship.position.y = parseFloat(ship.position.y);
-                  }
+        if (game) {
+            if (game.ships) {
+                _.each(game.ships, function (ship) {
+                    if (ship.position) {
+                        ship.position.x = parseFloat(ship.position.x);
+                        ship.position.y = parseFloat(ship.position.y);
+                    }
 
-                  if(ship.ship_count){
-                      ship.ship_count = parseInt(ship.ship_count);
-                  }
+                    if (ship.ship_count) {
+                        ship.ship_count = parseInt(ship.ship_count);
+                    }
 
-                  if(ship.owner == teamName){
-                      parsedGame.myShips.push(ship);
-                  }else if(ship.owner == "") {
-                      parsedGame.neutralShips.push(ship);
-                  }else {
-                      parsedGame.enemiesShips.push(ship);
-                  }
-              });
-          }
+                    if (ship.owner == teamName) {
+                        parsedGame.myShips.push(ship);
+                    } else if (ship.owner == "") {
+                        parsedGame.neutralShips.push(ship);
+                    } else {
+                        parsedGame.enemiesShips.push(ship);
+                    }
+                });
+            }
 
-          if(game.planets){
-              _.each(game.planets, function(planet){
-                  if(planet.position){
-                      planet.position.x = parseFloat(planet.position.x);
-                      planet.position.y = parseFloat(planet.position.y);
-                  }
+            if (game.planets) {
+                _.each(game.planets, function (planet) {
+                    if (planet.position) {
+                        planet.position.x = parseFloat(planet.position.x);
+                        planet.position.y = parseFloat(planet.position.y);
+                    }
 
-                  if(planet.ship_count){
-                      planet.ship_count = parseInt(planet.ship_count);
-                  }
+                    if (planet.ship_count) {
+                        planet.ship_count = parseInt(planet.ship_count);
+                    }
 
-                  if(planet.is_deathstar == "true"){
-                      planet.is_deathstar = true;
-                  }else{
-                      planet.is_deathstar = false;
-                  }
+                    if (planet.is_deathstar == "true") {
+                        planet.is_deathstar = true;
+                    } else {
+                        planet.is_deathstar = false;
+                    }
 
-                  if(planet.deathstar_charge){
-                      planet.deathstar_charge = parseFloat(planet.deathstar_charge);
-                  }
+                    if (planet.deathstar_charge) {
+                        planet.deathstar_charge = parseFloat(planet.deathstar_charge);
+                    }
 
-                  if(planet.size){
-                      planet.size = parseFloat(planet.size);
-                  }
+                    if (planet.size) {
+                        planet.size = parseFloat(planet.size);
+                    }
 
-                  if(planet.owner == teamName){
-                      parsedGame.myPlanets.push(planet);
-                  }else if(planet.owner == "") {
-                      parsedGame.neutralPlanets.push(planet);
-                  }else {
-                      parsedGame.enemiesPlanets.push(planet);
-                  }
-              });
-          }
-      }
+                    if (planet.owner == teamName) {
+                        parsedGame.myPlanets.push(planet);
+                    } else if (planet.owner == "") {
+                        parsedGame.neutralPlanets.push(planet);
+                    } else {
+                        parsedGame.enemiesPlanets.push(planet);
+                    }
+                });
+            }
+        }
 
-      return parsedGame;
-  }
+        return parsedGame;
+    }
 };
